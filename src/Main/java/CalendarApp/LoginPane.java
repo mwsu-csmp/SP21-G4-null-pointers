2f39f89312752a9d341cda8facf054405ae2277a
Josiah Randleman <69489368+jrandl@users.noreply.github.com>
2021-04-14 22:16:25 -0500

    Added more features to the Yes button in LoginPane.java. I am trying to add the login data into an arraylist that can be acessed by User class.

--- src/Main/java/CalendarApp/LoginPane.java
+++ src/Main/java/CalendarApp/LoginPane.java
@@ -1,22 +1,33 @@
 package CalendarApp;
 
-import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
-public class LoginPane extends FlowPane {
+import javax.naming.Name;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+
+public class LoginPane extends FlowPane  {
     // TODO: create a pane for user login, Josiah already has a good start at this!
 
     // TODO: possibly user could pick from list of created users, or create a new one. That way less typing is needed.
 
+    static ArrayList<Name> names;
+
+    static void nameDatabase() {
+        names = new ArrayList<>();
+        names.add((Name) new User("Josiah", LocalDate.now()));
+
+
+    }
 
     public LoginPane(){
         setPadding(new Insets(20, 20, 200, 20));
@@ -26,54 +37,50 @@ public class LoginPane extends FlowPane {
 
         Button y = new Button("Yes");
         Button n = new Button("No");
+        Button a = new Button("Add");
+        Button a1 = new Button("Add");
+
         Label l = new Label("");
+        TextField b = new TextField("initial text");
         EventHandler<ActionEvent> event = e -> {
-            l.setText("   Please enter your name:   ");
-            getChildren().addAll(new Label("First Name:"), new TextField(), new Label("MI"));
+            getChildren().addAll(new Label("Please enter your first and last name:"));
             TextField tfMi = new TextField();
-            tfMi.setPrefColumnCount(1);
-            getChildren().addAll(tfMi, new Label("Last Name:"), new TextField());
+            tfMi.setPrefColumnCount(10);
+            getChildren().addAll(tfMi, a);
+            getChildren().addAll(new Label("Please enter your date of birth:"));
+            DatePicker d = new DatePicker();
+            getChildren().addAll(d, a1);
+
+            a.setOnAction(new EventHandler<ActionEvent>() {
+                @Override public void handle(ActionEvent e) {
+                    Object full_name;
+                    full_name = tfMi.getText();
+                    names.add((Name) full_name);
+
+                    //System.out.println(full_name);
+                }
+            });
+            a1.setOnAction(new EventHandler<ActionEvent>() {
+                @Override public void handle(ActionEvent e) {
+                    Object date;
+                    date = d.getValue();
+                    DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("M/d/yyyy");
+                    LocalDate date1 = LocalDate.parse((CharSequence) date, dateFormat);
+                    //names.add((Name) date1);
+
+                }
+
+            });
+
 
         };
+
         l.setTranslateX(10);
         l.setTranslateY(-3);
+        a.setTranslateX(2);
+        a.setTranslateY(-1);
         y.setOnAction(event);
         getChildren().addAll(y, n, l);
 
     }
 }
-
-
-   /* public void start(Stage primaryStage) {
-        FlowPane pane = new FlowPane();
-        pane.setPadding(new Insets(11, 12, 13, 14));
-        pane.setHgap(5);
-        pane.setVgap(5);
-
-        pane.getChildren().addAll(new Label("Are you a first time user?"));
-        primaryStage.setTitle("creating buttons");
-        Button y = new Button("Yes");
-        Button n = new Button("No");
-        StackPane r = new StackPane();
-        y.setTranslateX(30);
-        y.setTranslateY(-102);
-        n.setTranslateX(70);
-        n.setTranslateY(-102);
-        Label l = new Label("Please enter your first name:");
-        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent e) {
-                l.setText("   button   selected    ");
-
-            }
-        };
-        r.getChildren().add(y);
-        r.getChildren().add(n);
-        r.getChildren().add(pane);
-        y.setOnAction(event);
-        r.getChildren().add(l);
-        primaryStage.setTitle("Calender");
-        primaryStage.setScene(new Scene(r, 300, 250));
-        primaryStage.show();
-
-    }
-} */
