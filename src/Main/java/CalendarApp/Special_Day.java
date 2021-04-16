5a2a0b1ff1f64cafa170f0f875ce46f0adfeb402
devina1 <damos2@missouriwestern.edu>
2021-04-16 17:04:04 -0500

    Created overloaded constructor that deserializes a string into a Special_Day object.

--- src/Main/java/CalendarApp/Special_Day.java
+++ src/Main/java/CalendarApp/Special_Day.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Special_Day {
     private String title;
@@ -33,6 +34,45 @@ public class Special_Day {
         this.location = location;
         this.isprivate = isprivate;
 
+    }
+
+    public Special_Day(String csvDesc) throws IllegalArgumentException{
+        try {
+            var fields = Arrays.asList(csvDesc.split(","));
+            var newfields = new ArrayList<String>();
+            if (fields.size() != 9) throw new IllegalArgumentException("Improper string length");
+
+            for (String field : fields) {
+                if ((field.charAt(0) != '\"' || field.charAt(field.length()-1) != '\"'))
+                    throw new IllegalArgumentException();
+                else newfields.add(field.strip().substring(1, field.length() - 1));
+            }
+
+            if (!newfields.get(5).equals("TRUE") && !newfields.get(5).equals("FALSE"))
+                throw new IllegalArgumentException("All Day Event boolean incorrectly formatted");
+            if (!newfields.get(8).equals("TRUE") && !newfields.get(8).equals("FALSE"))
+                throw new IllegalArgumentException("Private boolean incorrectly formatted");
+
+            title = newfields.get(0);
+            startdate = LocalDate.parse(newfields.get(1), DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+            if (!Boolean.parseBoolean(newfields.get(5))) {
+                starttime = LocalTime.parse(newfields.get(2), DateTimeFormatter.ofPattern("hh:mm a"));
+                endtime = LocalTime.parse(newfields.get(4), DateTimeFormatter.ofPattern("hh:mm a"));
+                isallday = false;
+            }
+            else {
+                starttime = null;
+                endtime = null;
+                isallday = true;
+            }
+            enddate = LocalDate.parse(newfields.get(3), DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+            description = newfields.get(6);
+            location = newfields.get(7);
+            isprivate = Boolean.parseBoolean(newfields.get(8));
+
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
     }
 
     public void update(String title, LocalDate startdate, LocalTime starttime, LocalDate enddate, LocalTime endtime, boolean isallday, String description, String location, boolean isprivate) {
@@ -70,18 +110,17 @@ public class Special_Day {
         if (isprivate) returnlist.add("TRUE");
         else returnlist.add("FALSE");
 
-        return "\"" + returnlist.get(0) + "\", \"" +
-                returnlist.get(1) + "\", \"" +
-                returnlist.get(2) + "\", \"" +
-                returnlist.get(3) + "\", \"" +
-                returnlist.get(4) + "\", \"" +
-                returnlist.get(5) + "\", \"" +
-                returnlist.get(6) + "\", \"" +
-                returnlist.get(7) + "\", \"" +
+        return "\"" + returnlist.get(0) + "\",\"" +
+                returnlist.get(1) + "\",\"" +
+                returnlist.get(2) + "\",\"" +
+                returnlist.get(3) + "\",\"" +
+                returnlist.get(4) + "\",\"" +
+                returnlist.get(5) + "\",\"" +
+                returnlist.get(6) + "\",\"" +
+                returnlist.get(7) + "\",\"" +
                 returnlist.get(8) + "\"";
     }
 
-    // TODO: Create an overloaded constructor that deserializes a String into a Special_Day object
 
 
 }
