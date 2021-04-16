e093b2d8d1d858690e1164be808a7f6272de0756
devina1 <damos2@missouriwestern.edu>
2021-04-16 17:04:39 -0500

    Added more tests to ensure nothing gets broken on accident.

--- src/test/java/CalendarAppTests/Special_DayTest.java
+++ src/test/java/CalendarAppTests/Special_DayTest.java
@@ -9,15 +9,22 @@ import static org.junit.Assert.*;
 
 public class Special_DayTest {
     @Test
-    public void testCreateSpecial_Day(){
-        LocalDate localDate = LocalDate.parse("2018-3-5");
+    public void testCreateSpecial_Day1(){
+        LocalDate localDate = LocalDate.parse("2017-03-05");
         Special_Day special_day1 = new Special_Day("Bill's Birthday", localDate, null, localDate, null, true, "Bill Harris BD", "Atlanta", true);
         assertNotNull(special_day1);
+    }
+    @Test
+    public void testCreateSpecial_Day2(){
+        String string = "\"Bill's Birthday\",\"03/05/2017\",\"\",\"03/05/2017\",\"\",\"TRUE\",\"Bill Harris BD\",\"Atlanta\",\"TRUE\"";
+        Special_Day special_day1 = new Special_Day(string);
+        assertNotNull(special_day1);
+        assertTrue(special_day1.serialize().equals(string));
     }
     @Test
     public void testSerializeSpecial_Day(){
         LocalDate localDate = LocalDate.parse("2017-03-05");
         Special_Day special_day1 = new Special_Day("Bill's Birthday", localDate, null, localDate, null, true, "Bill Harris BD", "Atlanta", true);
-        assertTrue(special_day1.serialize().equals("\"Bill's Birthday\", \"03/05/2017\", \"\", \"03/05/2017\", \"\", \"TRUE\", \"Bill Harris BD\", \"Atlanta\", \"TRUE\""));
+        assertTrue(special_day1.serialize().equals("\"Bill's Birthday\",\"03/05/2017\",\"\",\"03/05/2017\",\"\",\"TRUE\",\"Bill Harris BD\",\"Atlanta\",\"TRUE\""));
     }
 }
