fe263ba77e05b6b59c7b1154de1443e279ab9f06
devina1 <damos2@missouriwestern.edu>
2021-04-16 17:02:43 -0500

    Added "Add Event" button that creates an addEventPane

--- src/Main/java/CalendarApp/CalendarPane.java
+++ src/Main/java/CalendarApp/CalendarPane.java
@@ -1,25 +1,30 @@
 package CalendarApp;
 
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 public class CalendarPane extends BorderPane {
     private final Calendar CalendarBody;
     private final Label datetext;
     private final Button nextmonth;
     private final Button previousmonth;
+    private final Button addevent;
 
-    public CalendarPane(int month, int year){
-        CalendarBody = new Calendar(month, year);
+    public CalendarPane(int month, int year, User user){
+        CalendarBody = new Calendar(month, year, user);
         datetext = new Label(CalendarBody.dateToString());
         nextmonth = new Button(">");
         previousmonth = new Button("<");
+        addevent = new Button("Add Event");
 
         HBox topSector = new HBox(previousmonth, datetext, nextmonth);
+        HBox bottomSector = new HBox(addevent);
 
         // TODO: Implement User parameter into CalendarPane for Special_Days
 
@@ -31,6 +36,13 @@ public class CalendarPane extends BorderPane {
         previousmonth.setOnAction(event -> {
             CalendarBody.moveMonthBackwards();
             datetext.setText(CalendarBody.dateToString());
+        });
+
+        addevent.setOnAction(event -> {
+            Scene scene = new Scene(new addEventPane(user));
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
         });
 
         previousmonth.setTooltip(new Tooltip("Goes back a month"));
@@ -46,5 +58,6 @@ public class CalendarPane extends BorderPane {
 
         setCenter(CalendarBody);
         setTop(topSector);
+        setBottom(bottomSector);
     }
 }
