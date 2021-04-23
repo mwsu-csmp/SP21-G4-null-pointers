c73dfab58ce83dcb17a8d332a089302c7093d955
devina1 <damos2@missouriwestern.edu>
2021-04-22 23:23:36 -0500

    Added description tooltips to eventbuttons

--- src/Main/java/CalendarApp/EventButton.java
+++ src/Main/java/CalendarApp/EventButton.java
@@ -2,12 +2,17 @@ package CalendarApp;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 import javafx.stage.Stage;
 
 public class EventButton extends Button {
 
     public EventButton(User user ,Special_Day special_day) {
         super(special_day.getTitle());
+
+        if (!special_day.getDescription().equals(""))
+            setTooltip(new Tooltip(special_day.getDescription()));
+
         setOnAction(actionEvent -> {
             Stage stage = new Stage();
             Scene scene = new Scene(new EventPane(user, closestage -> stage.close(), special_day));
